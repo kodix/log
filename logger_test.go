ffb7ca80d118940a0b6b10e1d52b0259a188c7e5
abramdi <da@kodix.ru>
2018-04-03 13:25:20 +0300

    logger test (race detection, verbosity setter test)

---
+++ logger_test.go
@@ -0,0 +1,47 @@
+package log
+
+import (
+	"log"
+	"testing"
+	"os"
+	"runtime"
+)
+
+func TestRace(t *testing.T) {
+	out, err := os.Open(os.DevNull)
+	if err != nil {
+		t.Fatal(err)
+	}
+	SetOutput(out)
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go func(){
+			SetVerbosity(Debug)
+			Debugln("")
+			Debugf("")
+			SetVerbosity(Error)
+			SetVerbosity(Warning)
+			Warningln("")
+			Warningf("")
+			_ = std.Verbosity()
+			SetOutput(out)
+			Errorln("")
+			Errorf("")
+			SetPrefix("")
+			SetVerbosity(Info)
+			SetFlags(log.LstdFlags | log.Lshortfile)
+			Infoln("")
+			Infof("")
+		}()
+	}
+}
+
+func TestLogger_SetVerbosity(t *testing.T) {
+	var name = "Verbosity setter"
+	t.Run(name, func(t *testing.T) {
+		l := New(os.Stdout, "", Error, log.LstdFlags)
+		l.SetVerbosity(Info)
+		if l.Verbosity() != Info {
+			t.Errorf("%s: want: %v, got: %v", name, Info, l.Verbosity())
+		}
+	})
+}
